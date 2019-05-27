bcd8d9480f6607ca0ad2df9821bb84f0a972578c
Txus75 <txus75h@yahoo.es>
2019-05-27 09:05:49 +0200

    Ejercicio básico de SASS

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var squareContainer = $("#squares");
+var squareNum = 25;
+for (let i = 0; i < squareNum; i++) {
+    squareContainer.append('<div class="square"><div class="content"><p>IT Academy</p></div></div>');
+}
